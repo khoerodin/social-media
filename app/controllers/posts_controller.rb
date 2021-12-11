e0f7da5a32c0f23ed0f70c11df04c592c20b7e96
Khoerodin <khoerodin@live.com>
2021-12-12 03:22:48 +0700

    Final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
   end
 
   # GET /posts/1 or /posts/1.json
@@ -25,9 +26,16 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { redirect_to posts_path }
         format.json { render :show, status: :created, location: @post }
       else
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.replace(
+            @post,
+            partial: 'posts/form',
+            locals: { post: @post }
+          )
+        }
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
